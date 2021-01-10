8f39735ca2b052b86f60708d0013fa058f15823c
mTarik12 <72448312+mTarik12@users.noreply.github.com>
2021-01-10 20:53:49 +0200

    pretified

--- src/index.js
+++ src/index.js
@@ -1,68 +1,12 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { of, interval, concat, Subject, merge, empty } from "rxjs";
-import {
-    scan,
-    startWith,
-    share,
-    filter,
-    mapTo,
-    switchMap
-} from "rxjs/operators";
+
 import "./styles.css";
 import constants from './constants';
-import stopWatchCore from './stopWatchCore';
-
-const actions$ = new Subject();
-const start$ = actions$.pipe(
-    filter((action) => action === constants.START),
-    mapTo(constants.START)
-);
-const wait$ = actions$.pipe(
-    filter((action) => action === constants.WAIT),
-    mapTo(constants.WAIT)
-);
-
-const stop$ = actions$.pipe(
-    filter((action) => action === constants.STOP),
-    mapTo(constants.STOP)
-);
-
-const reset$ = actions$.pipe(
-    filter((action) => action === constants.RESET),
-    mapTo(constants.RESET)
-);
-
-const interval$ = interval(10).pipe(mapTo(1));
-
-const COUNTDOWN_INIT_VALUE = { hr: 0, min: 0, sec: 0, ms: 0 };
-const stopwatch$ = merge(start$, wait$, stop$, reset$)
-    .pipe(
-        startWith(constants.INIT),
-        switchMap((val) => {
-            if (val === constants.WAIT) {
-                return empty();
-            } else if (val === constants.START) {
-                return interval$;
-            } else if (val === constants.INIT || val === constants.STOP) {
-                return of(false);
-            } else if (val === constants.RESET) {
-                return concat(of(false), interval$);
-            }
-        }),
-        scan(
-            (acc, curr) => {
-                if (!curr) {
-                    return { hr: 0, min: 0, sec: 0, ms: 0 };
-                };
-                return { ...stopWatchCore(acc) };
-            },
-            COUNTDOWN_INIT_VALUE
-        ))
-    .pipe(share());
+import { actions$, stopwatch$ } from './stopwatchObservable'
 
 let clickedOnce = false;
-function onWaitClick() {
+const onWaitClick = () => {
     if (clickedOnce) {
         actions$.next(constants.WAIT);
         clickedOnce = false;
@@ -74,7 +18,8 @@ function onWaitClick() {
     };
 };
 
-function App() {
+const App = () => {
+    
     const [stopwatchState, setStopwatchState] = useState();
     useEffect(() => {
         const stopwatchSub = stopwatch$.subscribe(setStopwatchState);
